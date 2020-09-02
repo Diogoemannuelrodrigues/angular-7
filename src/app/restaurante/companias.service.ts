8446a26126c8c676c8bc09e677d559dafafea962
Diogo Emannuel Ferreira ROdrigues <diogoemannuel.rodrigues1@gmail.com>
2020-09-02 09:53:14 -0300

    Error Handler

--- src/app/restaurante/companias.service.ts
+++ src/app/restaurante/companias.service.ts
@@ -4,6 +4,8 @@ import {MEAT_API} from '../app.api'
 import {Http} from '@angular/http'
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+import {ErrorHandler} from '../app.error-handler'
 
 @Injectable()
 export class CompaniasService{
@@ -13,5 +15,6 @@ export class CompaniasService{
     companias(): Observable<Companias[]>{
         return this.http.get(`${MEAT_API}/restaurants`)
         .map(response => response.json())
+        .catch(ErrorHandler.handleError)
     }
 }
